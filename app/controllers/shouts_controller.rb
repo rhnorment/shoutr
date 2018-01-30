22ab7e16df775644011694a279b5388925b30938
Hunt Norment <hunt@sherpa.red>
2018-01-30 16:48:19 -0500

    rendering shouts in dashboard

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,21 @@
+class ShoutsController < ApplicationController
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to root_url, redirect_options_for(shout)
+  end
+
+  private
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for(shout)
+    if shout.persisted?
+      { notice: 'Shout successful' }
+    else
+      { alert: 'Could not shout' }
+    end
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
 
   root to: 'homes#show'
 
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resources :shouts, only: [:create]
+
+  resources :passwords, controller: 'clearance/passwords', only: [:create, :new]
   resource :session, only: [:create]
 
   resources :users, only: [:create] do
-    resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+    resource :password, controller: 'clearance/passwords', only: [:create, :edit, :update]
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
+  get '/sign_in' => 'sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'users#new', as: 'sign_up'
 end
